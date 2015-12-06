15f73718299f5f778da4a6c979144e1f960d7ea9
dman95 <dm@n95.me>
2015-12-06 17:04:27 +0400

    random, 2 hours and small fixes

--- gensparse/generate_sparse.c
+++ gensparse/generate_sparse.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "mpi.h"
+#include <time.h>
 
 //generate square sparse matrix and vector by size
 int main(int argc, char **argv)
@@ -16,14 +17,26 @@ int main(int argc, char **argv)
     sscanf(argv[1], "%d", &size);
     sscanf(argv[2], "%d", &coef);
 
-    //generate vector
-    vector *v = vector_gen(size);
-    vector_save(v, vectorname);
-    vector_delete(v);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    srand(7 * time(NULL) + 1046527 * rank);
 
+    double start = -MPI_Wtime();
+    if (!rank) {
+        //generate vector
+        vector *v = vector_gen(size);
+        vector_save(v, vectorname);
+        vector_delete(v);
+    }
+    
     //generate sparse matrix
     mpi_sparse_matrix_gen_and_save(size, size, matrixname, 1.0 * coef / size);
     
+    if (!rank) {
+        printf("Time: %f\n", MPI_Wtime() + start);
+    }
+
     MPI_Finalize();
     return 0;
 }
--- show.py
+++ show.py
@@ -3,10 +3,16 @@
 import sys
 from_count = int(sys.argv[1])
 to = int(sys.argv[2])
+n = int(sys.argv[3])
+sparse = len(sys.argv) > 4
+if sparse:
+    filetemplate = 'out%d_%d_sparse.txt'
+else:
+    filetemplate = 'out%d_%d.txt'
 
 i = from_count
 while i <= to:
-    f = open('out%d.txt' % (i), 'r')
+    f = open(filetemplate % (i, n), 'r')
     print('%d %f' % (i, float(f.read().split()[1])))
     f.close()
     i *= 2
