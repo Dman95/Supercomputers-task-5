b7a3ee7c310d33c22cef0cb35e274f8addcde780
dman95 <dm@n95.me>
2015-12-07 22:57:34 +0400

    watch improved

--- watch.py
+++ watch.py
@@ -5,13 +5,16 @@ import os, time, sys
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 
-while True:
-    f = os.popen('llq -W')
-    s = f.read()
-    print s,
-    f.close()
-    time.sleep(1)
-    for i in range(s.count('\n')):
-        print CURSOR_UP_ONE + ERASE_LINE, 
-    print '\r',
+try:
+    while True:
+        f = os.popen('llq -W')
+        s = f.read()
+        print s,
+        f.close()
+        time.sleep(1)
+        for i in range(s.count('\n')):
+            print CURSOR_UP_ONE + ERASE_LINE, 
+        print '\r',
+except KeyboardInterrupt:
+    pass
 
