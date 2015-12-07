3381c69d4ab094c4240df30fced60bc12092c4a0
dman95 <dm@n95.me>
2015-12-07 18:46:56 +0400

    show.py fixed

--- show.py
+++ show.py
@@ -3,18 +3,15 @@
 import sys
 from_count = int(sys.argv[1])
 to = int(sys.argv[2])
-n = int(sys.argv[3])
-sparse = len(sys.argv) > 4
-if sparse:
-    filetemplate = 'out%d_%d_sparse.txt'
-else:
-    filetemplate = 'out%d_%d.txt'
+A = sys.argv[3]
+B = sys.argv[4]
+filetemplate = 'out%s_%s' % (A, B) + '_%d.txt'
 
 i = from_count
 while i <= to:
     try:
-        f = open(filetemplate % (i, n), 'r')
-        time = float(f.read().split()[1])
+        f = open(filetemplate % (i), 'r')
+        time = max([float(s.split()[1]) for s in f.read().split('\n') if s != ''])
         f.close()
     except:
         time = 0
